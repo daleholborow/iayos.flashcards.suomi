1a035d585d4c7cd4ab850a7f2f8e004a1ed4e868
dale.holborow <daleholborow@hotmail.com>
2017-05-24 02:04:27 +0300

    merge got wanged?

--- frontend/app/src/script.ts
+++ frontend/app/src/script.ts
@@ -10,32 +10,45 @@
 	store = getStore();
 
 
+	/*
+		
+	*/
 	function saveStore(): void {
 		localStorage.setItem(store_key, JSON.stringify(store));
 	}
 
 
+	/*
+		
+	*/
 	function getStore(): flashcards.DeckStore {
 		if (localStorage.getItem(store_key) === null) {
 			store = new flashcards.DeckStore();
 			saveStore();
 		}
 		else {
+
 			store = JSON.parse(localStorage.getItem(store_key));
+			console.log("loaded store from localstorage", store);
 		}
 		return store;
 	}
 
 
+	/*
+		
+	*/
 	function updateSelectedDeck(deckCode: string): void {
 		store.activeDeckCode = deckCode;
 		store.activeCardIndex = 0;
 
-		if (!!store.decks[deckCode] == false) {
+		if (!!getActiveDeck() == false) {
+			console.log("trying to update selected and didnt find in cache");
 			$.ajaxSetup({ "async": false });
 			$.getJSON("/data/" + deckCode + ".json", function (json) {
-				store.decks[deckCode] = json;
+				store.decks[store.decks.length] = json;
 				saveStore();
+				console.log("saved a new deck into store", store);
 				$.ajaxSetup({ "async": false });
 			});
 		} else {
@@ -44,14 +57,34 @@
 	}
 
 
+	/*
+		
+	*/
 	function flipToBack(): void {
-		//$(".front").css("visibility", "hidden");
-		//$(".back").css("visibility", "visible");
-		$("#card").flip('toggle');
-		//$("#card").flip();
+
+		// Couldnt pass options to callback in flip, 
+		$('.answer-button').css('opacity', '0.0');
+
+		// var flip = $("#card").data("flip-model");
+		// // e.g. to see currect flip state
+		// flip.isFlipped;
+
+		$("#card").flip('toggle', ensureAnswerButtonsVisible);
 	}
 
 
+	/*
+		
+	*/
+	function ensureAnswerButtonsVisible(): void {
+		// make the answer buttons show now... they are misbehaving
+		$('.answer-button').css('opacity', '1.0');  // visible maximum
+	}
+
+
+	/*
+		
+	*/
 	$(document).on('click', '.answer-button', function () {
 		let chosenAnswerId = $(this).data("answer-card-id");
 		let isCorrectAnswer: boolean = chooseAnswer(chosenAnswerId);
@@ -73,6 +106,9 @@
 	});
 
 
+	/*
+		
+	*/
 	function shake(elementId): void {
 		var div = document.getElementById(elementId);
 		var interval = 100;
@@ -88,30 +124,46 @@
 	}
 
 
+	/*
+		
+	*/
 	function chooseAnswer(selectedCardId: number): boolean {
 
+		let activeDeck = getActiveDeck();
 		// Because we deserialized store from JSON some properties might not be set correctly
-		if (!!(store.decks[store.activeDeckCode].cards[store.activeCardIndex].score) == false) {
-			store.decks[store.activeDeckCode].cards[store.activeCardIndex].score = 0;
+		if (!!(activeDeck.cards[store.activeCardIndex].r) == false) {
+			activeDeck.cards[store.activeCardIndex].r = 0;
+		}
+		if (!!(activeDeck.cards[store.activeCardIndex].w) == false) {
+			activeDeck.cards[store.activeCardIndex].w = 0;
 		}
 
-		let scoreIncrement: number = 0;
 		let isCorrectAnswer: boolean = validateAnswer(selectedCardId);
 		if (isCorrectAnswer) {
-			scoreIncrement = 1;
+			activeDeck.cards[store.activeCardIndex].r += 1;
 		}
 		else {
-			scoreIncrement = -1;
+			activeDeck.cards[store.activeCardIndex].w += 1;
 		}
 
-		store.decks[store.activeDeckCode].cards[store.activeCardIndex].score += scoreIncrement;
 		saveStore();
-		console.log("Updated the score to  ", store.decks[store.activeDeckCode].cards[store.activeCardIndex].score);
+
+		console.log("Updated the score to  ",
+			activeDeck.cards[store.activeCardIndex].r,
+			activeDeck.cards[store.activeCardIndex].w,
+			activeDeck.cards[store.activeCardIndex].r / (
+				activeDeck.cards[store.activeCardIndex].r +
+				activeDeck.cards[store.activeCardIndex].w
+			)
+		);
 
 		return isCorrectAnswer;
 	}
 
 
+	/*
+		
+	*/
 	function validateAnswer(selectedCardId: number): boolean {
 		// get the correct answer
 		let correctCard: flashcards.Card = getActiveDeck().cards[store.activeCardIndex];
@@ -120,61 +172,123 @@
 	}
 
 
+	/*
+		
+	*/
 	function navToHome(): void {
 		$.mobile.pageContainer.pagecontainer("change", "#page-home", {
-			transition: "fade", changeHash: false, reload: true, allowSamePageTransition: false
+			transition: "flip", changeHash: false, reload: true, allowSamePageTransition: false
 		});
 	}
 
 
+	/*
+		
+	*/
 	function navToScoreboard(): void {
 		$.mobile.pageContainer.pagecontainer("change", "#page-scoreboard", {
-			transition: "slide", changeHash: false, reload: true, allowSamePageTransition: false
+			transition: "flip", changeHash: false, reload: false, allowSamePageTransition: false
 		});
 	}
 
 
-	function goToScoreBoard(): void {
-		$.mobile.pageContainer.pagecontainer("change", "#page-scoreboard", {
-			transition: "fade", changeHash: false, reload: false, allowSamePageTransition: false
-		});
-	}
-
-
+	/*
+		
+	*/
 	$("#btn-scoreboard").on('click', function () {
 
-		goToScoreBoard();
+		navToScoreboard();
 
 	});
 
 
+	/*
+		
+	*/
 	$(document).on('click', '.select-deck', function () {
 
 		let selectedDeckCode = $(this).data("deck-code");
 		updateSelectedDeck(selectedDeckCode);
 
 		// Navigate to the deck view page
-		$.mobile.pageContainer.pagecontainer("change", "#page-deck", { transition: "flow", changeHash: false, reload: false, allowSamePageTransition: false });
+		$.mobile.pageContainer.pagecontainer("change", "#page-deck", { transition: "flip", changeHash: false, reload: false, allowSamePageTransition: false });
 
 	});
 
 
+	/*
+		
+	*/
 	$("#card").flip({
 		trigger: 'manual'
 	});
- 
 
+
+	/*
+		
+	*/
 	$("#toggle-flashcard").click(function () {
 		flipToBack();
 	});
 
 
+	/*
+		
+	*/
 	function getActiveDeck(): flashcards.Deck {
-		let deck = store.decks[store.activeDeckCode];
+		let deck = store.decks.find(x => x.code == store.activeDeckCode);
 		return deck;
 	}
 
 
+	/*
+		
+	*/
+	function calculateAccuracy(right: number, wrong: number): number {
+		// console.log("right:" + !!right, right);
+		// console.log("wrong:" + !!wrong, wrong);
+		let x = (!!right) ? right : 0;
+		let y = (!!wrong) ? wrong : 0;
+		// console.log("i have x and y", x, y);
+		//if (!!right == false || !!wrong == false) return 0;
+		if (x == 0 && y == 0) return 0;
+		return Math.floor((right / (right + wrong)) * 100);
+	}
+
+
+	/*
+
+	 */
+	$(document).on('pagebeforeshow', "#page-scoreboard", function () {
+		let activeDeck = getActiveDeck();
+		$("#hd-scoreboard-deckname").text(activeDeck.title);
+
+		let myTemplatevalue: string = "dale replace this";
+		let scoresTbl = $("#tbody-cardscores");
+		scoresTbl.empty();
+		for (let card of activeDeck.cards) {
+			let accuracy = calculateAccuracy(card.r, card.w);
+			scoresTbl.append(`
+				<tr>
+					<td>${card.f}</td>
+					<td rowspan="2">${accuracy}%</td>
+					<td rowspan="2" >
+						<a href="#" class="ui-btn ui-corner-all ui-icon-delete ui-btn-icon-notext">Delete</a>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="1">
+						${card.b}
+					</td>
+				</tr>
+			`);
+		}
+	});
+
+
+	/*
+		
+	*/
 	$(document).on('pagebeforeshow', '#page-deck', function () {
 
 		$("#page-deck-header").html(getActiveDeck().title);
@@ -191,6 +305,9 @@
 	});
 
 
+	/*
+		
+	*/
 	function renderRandomCard(): void {
 
 		// Always set to unflipped initially
@@ -227,6 +344,9 @@
 	}
 
 
+	/*
+		
+	*/
 	function getRandomCardIndex(usedCardIndices: number[]): number {
 		let randomIndex: number = -1;
 		let activeDeck = getActiveDeck();
