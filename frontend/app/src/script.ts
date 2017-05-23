ea57b22681cfea2fc1cad3e4b431bbbbd3097dfd
dale.holborow <daleholborow@hotmail.com>
2017-05-24 02:03:05 +0300

    nice-back: now its all starting to look and work quite simply

--- frontend/app/src/script.ts
+++ frontend/app/src/script.ts
@@ -10,18 +10,24 @@
 	store = getStore();
 
 
+	/*
+		
+	*/
 	function saveStore(): void {
 		localStorage.setItem(store_key, JSON.stringify(store));
 	}
 
 
+	/*
+		
+	*/
 	function getStore(): flashcards.DeckStore {
 		if (localStorage.getItem(store_key) === null) {
 			store = new flashcards.DeckStore();
 			saveStore();
 		}
 		else {
-			
+
 			store = JSON.parse(localStorage.getItem(store_key));
 			console.log("loaded store from localstorage", store);
 		}
@@ -29,6 +35,9 @@
 	}
 
 
+	/*
+		
+	*/
 	function updateSelectedDeck(deckCode: string): void {
 		store.activeDeckCode = deckCode;
 		store.activeCardIndex = 0;
@@ -48,6 +57,9 @@
 	}
 
 
+	/*
+		
+	*/
 	function flipToBack(): void {
 
 		// Couldnt pass options to callback in flip, 
@@ -61,12 +73,18 @@
 	}
 
 
+	/*
+		
+	*/
 	function ensureAnswerButtonsVisible(): void {
 		// make the answer buttons show now... they are misbehaving
 		$('.answer-button').css('opacity', '1.0');  // visible maximum
 	}
 
 
+	/*
+		
+	*/
 	$(document).on('click', '.answer-button', function () {
 		let chosenAnswerId = $(this).data("answer-card-id");
 		let isCorrectAnswer: boolean = chooseAnswer(chosenAnswerId);
@@ -88,6 +106,9 @@
 	});
 
 
+	/*
+		
+	*/
 	function shake(elementId): void {
 		var div = document.getElementById(elementId);
 		var interval = 100;
@@ -103,6 +124,9 @@
 	}
 
 
+	/*
+		
+	*/
 	function chooseAnswer(selectedCardId: number): boolean {
 
 		let activeDeck = getActiveDeck();
@@ -121,14 +145,14 @@
 		else {
 			activeDeck.cards[store.activeCardIndex].w += 1;
 		}
-		
+
 		saveStore();
-		
-		console.log("Updated the score to  ", 
+
+		console.log("Updated the score to  ",
 			activeDeck.cards[store.activeCardIndex].r,
 			activeDeck.cards[store.activeCardIndex].w,
 			activeDeck.cards[store.activeCardIndex].r / (
-				activeDeck.cards[store.activeCardIndex].r + 
+				activeDeck.cards[store.activeCardIndex].r +
 				activeDeck.cards[store.activeCardIndex].w
 			)
 		);
@@ -137,6 +161,9 @@
 	}
 
 
+	/*
+		
+	*/
 	function validateAnswer(selectedCardId: number): boolean {
 		// get the correct answer
 		let correctCard: flashcards.Card = getActiveDeck().cards[store.activeCardIndex];
@@ -145,6 +172,9 @@
 	}
 
 
+	/*
+		
+	*/
 	function navToHome(): void {
 		$.mobile.pageContainer.pagecontainer("change", "#page-home", {
 			transition: "flip", changeHash: false, reload: true, allowSamePageTransition: false
@@ -152,6 +182,9 @@
 	}
 
 
+	/*
+		
+	*/
 	function navToScoreboard(): void {
 		$.mobile.pageContainer.pagecontainer("change", "#page-scoreboard", {
 			transition: "flip", changeHash: false, reload: false, allowSamePageTransition: false
@@ -159,6 +192,9 @@
 	}
 
 
+	/*
+		
+	*/
 	$("#btn-scoreboard").on('click', function () {
 
 		navToScoreboard();
@@ -166,6 +202,9 @@
 	});
 
 
+	/*
+		
+	*/
 	$(document).on('click', '.select-deck', function () {
 
 		let selectedDeckCode = $(this).data("deck-code");
@@ -177,62 +216,79 @@
 	});
 
 
+	/*
+		
+	*/
 	$("#card").flip({
 		trigger: 'manual'
 	});
 
 
+	/*
+		
+	*/
 	$("#toggle-flashcard").click(function () {
 		flipToBack();
 	});
 
 
+	/*
+		
+	*/
 	function getActiveDeck(): flashcards.Deck {
 		let deck = store.decks.find(x => x.code == store.activeDeckCode);
 		return deck;
 	}
 
 
-	function calculateAccuracy(right : number, wrong : number) : number {
-		console.log("right:" + !!right, right);
-		console.log("wrong:" + !!wrong, wrong);
+	/*
+		
+	*/
+	function calculateAccuracy(right: number, wrong: number): number {
+		// console.log("right:" + !!right, right);
+		// console.log("wrong:" + !!wrong, wrong);
 		let x = (!!right) ? right : 0;
 		let y = (!!wrong) ? wrong : 0;
-		console.log("i have x and y", x, y);
+		// console.log("i have x and y", x, y);
 		//if (!!right == false || !!wrong == false) return 0;
 		if (x == 0 && y == 0) return 0;
 		return Math.floor((right / (right + wrong)) * 100);
 	}
 
 
-	$(document).on('pagebeforeshow', "#page-scoreboard", function () {
+	/*
 
+	 */
+	$(document).on('pagebeforeshow', "#page-scoreboard", function () {
 		let activeDeck = getActiveDeck();
 		$("#hd-scoreboard-deckname").text(activeDeck.title);
 
 		let myTemplatevalue: string = "dale replace this";
-		let scoresList = $("#list-card-scores");
-		scoresList.empty();
+		let scoresTbl = $("#tbody-cardscores");
+		scoresTbl.empty();
 		for (let card of activeDeck.cards) {
-			console.log(card); // , "string", false  
-
 			let accuracy = calculateAccuracy(card.r, card.w);
-
-			scoresList.append(` 
-				<li> 
-					<a href="#"> 
-						<h2>${card.f} || ${card.b}</h2> 
-						Accuracy: ${accuracy}%
-					</a>
-					<a href="#" data-rel="popup">reset score</a>
-				</li>
+			scoresTbl.append(`
+				<tr>
+					<td>${card.f}</td>
+					<td rowspan="2">${accuracy}%</td>
+					<td rowspan="2" >
+						<a href="#" class="ui-btn ui-corner-all ui-icon-delete ui-btn-icon-notext">Delete</a>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="1">
+						${card.b}
+					</td>
+				</tr>
 			`);
 		}
-
-
-		scoresList.listview('refresh');
 	});
 
+
+	/*
+		
+	*/
 	$(document).on('pagebeforeshow', '#page-deck', function () {
 
 		$("#page-deck-header").html(getActiveDeck().title);
@@ -249,6 +305,9 @@
 	});
 
 
+	/*
+		
+	*/
 	function renderRandomCard(): void {
 
 		// Always set to unflipped initially
@@ -285,6 +344,9 @@
 	}
 
 
+	/*
+		
+	*/
 	function getRandomCardIndex(usedCardIndices: number[]): number {
 		let randomIndex: number = -1;
 		let activeDeck = getActiveDeck();
