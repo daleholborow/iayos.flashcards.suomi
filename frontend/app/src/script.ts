582d2795ec45537c9c9e9b2de99a846a53498f98
dale.holborow <daleholborow@hotmail.com>
2017-05-24 19:25:42 +0300

    added functionality to reset scores for individual cards

--- frontend/app/src/script.ts
+++ frontend/app/src/script.ts
@@ -267,21 +267,38 @@
 			scoresTbl.append(`
 				<tr>
 					<td>${card.f}</td>
-					<td rowspan="2"><div class="center">${accuracy}%</div></td>
+					<td rowspan="2"><div class="center" id="card-id-accuracy${card.id}">${accuracy}%</div></td>
 					<td rowspan="2">
 						<div class="center">
-							<a href="#" class="ui-btn ui-corner-all ui-icon-delete ui-btn-icon-notext">Delete</a>
+							<a href="#" data-card-id="${card.id}" class="clear-card ui-btn ui-icon-delete ui-btn-icon-notext ui-corner-all">Reset</a>
 						</div>
 					</td>
 				</tr>
 				<tr>
 					<td colspan="1">
-						${card.b}
+						${card.b} 
 					</td>
 				</tr>
 			`);
 		}
+		
+		// Add the appropriate handler to dynamically added reset buttons
+		$(".clear-card").on('click', function () {
+			let cardId : number = Number($(this).attr("data-card-id"));
+			resetCardScore(cardId);
+			$("#card-id-accuracy" + cardId).text("0%");
+		});
 	});
+
+
+	/*
+		
+	*/
+	function resetCardScore(cardId : number) : void {
+		getActiveDeck().cards.find(c => c.id == cardId).r = 0;
+		getActiveDeck().cards.find(c => c.id == cardId).w = 0;
+		saveStore();
+	}
 
 
 	/*
